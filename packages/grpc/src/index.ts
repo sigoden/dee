5b2eaa27f2bc086d246094221b52f590db014a16
Sigoden Huang <sigoden@gmail.com>
2018-09-03 17:28:24 +0800

    (grpc) add test

--- packages/grpc/src/index.ts
+++ packages/grpc/src/index.ts
@@ -5,27 +5,14 @@ import * as grpc from "grpc";
 declare namespace DeeGRPC {
   export interface Service extends Dee.Service {
     server?: grpc.Server;
-    clients?: GrpcClientMap;
+    clients?: ClientMap;
   }
 
   export interface ServiceOptions extends Dee.ServiceOptions {
     args: Args;
   }
 
-  export interface GrpcClientMap {
-    [k: string]: GrpcClient;
-  }
-
-  interface GrpcClient extends grpc.Client {
-    call: (
-      name: string,
-      args: any,
-      metdata?: Metadata,
-      callback?: (data: any) => void
-    ) => Promise<any> | any;
-  }
-
-  interface Args {
+  export interface Args extends Dee.Args {
     // path to server proto file
     serverProtoFile?: string;
     // listenning host of server
@@ -42,44 +29,50 @@ declare namespace DeeGRPC {
     havePermision?: CheckPermisionFunc;
   }
 
+  export interface ClientMap {
+    [k: string]: Client;
+  }
+
+  export interface Client extends grpc.Client {
+    call: (name: string, args: any, metdata?: grpc.Metadata) => Promise<any>;
+  }
+
   export type CheckPermisionFunc = (serviceName: string, id: string) => boolean;
 
   export interface HandlerFuncMap {
     [k: string]: HandlerFunc;
   }
 
-  export type HandlerFunc = (
-    ctx: Context,
-    callback?: (result: any) => void
-  ) => Promise<void> | void;
+  export type HandlerFunc = (ctx: Context) => Promise<any>;
 
   export interface Context {
     request: any;
-    metadata: Metadata;
+    metadata: grpc.Metadata;
     srvs: Dee.ServiceGroup;
-  }
-
-  export interface Metadata {
-    origin: string;
   }
 }
 
 async function DeeGRPC(
-  options: DeeGRPC.ServiceOptions
+  ctx: Dee.ServiceInitializeContext,
+  args: DeeGRPC.Args
 ): Promise<DeeGRPC.Service> {
-  const { serverProtoFile, clientProtoFile } = options.args;
+  const { serverProtoFile, clientProtoFile } = args;
   const srv: DeeGRPC.Service = {};
   if (serverProtoFile) {
-    srv.server = await createServer(options);
+    srv.server = await createServer(ctx, args);
   }
   if (serverProtoFile) {
-    srv.clients = await createClients(options);
+    srv.clients = await createClients(ctx, args);
   }
   return srv;
 }
 
+const DEFAULT_HOST = "127.0.0.1";
+const DEFAULT_PORT = "4444";
+
 async function createServer(
-  options: DeeGRPC.ServiceOptions
+  ctx: Dee.ServiceInitializeContext,
+  args: DeeGRPC.Args
 ): Promise<grpc.Server> {
   const {
     serverProtoFile,
@@ -87,8 +80,8 @@ async function createServer(
     havePermision = () => true,
     serverHost = "127.0.0.1",
     serverPort = "4444"
-  } = options.args;
-  const { ns, name } = options.srvs.$config;
+  } = args;
+  const { ns, name } = ctx.srvs.$config;
   const protoRoot = loadProtoFile(serverProtoFile);
   let proto: grpc.GrpcObject;
   try {
@@ -96,9 +89,8 @@ async function createServer(
   } catch (err) {
     throw new Error(`no grpc service at ${ns}.${name}`);
   }
-  shimHandlers(serverHandlers, havePermision);
   const server = new grpc.Server();
-  server.addService(proto.service, serverHandlers);
+  server.addService(proto.service, shimHandlers(serverHandlers, havePermision));
   // TODO support more credential
   server.bind(
     `${serverHost}:${serverPort}`,
@@ -111,28 +103,37 @@ async function createServer(
 function shimHandlers(
   handlers: DeeGRPC.HandlerFuncMap,
   havePermision: DeeGRPC.CheckPermisionFunc
-): void {
+) {
+  let result = {};
   Object.keys(handlers).forEach(id => {
     const fn = handlers[id];
-    handlers[id] = (ctx: DeeGRPC.Context, callback?: (result: any) => void) => {
-      if (!havePermision(ctx.metadata.origin, id)) {
-        callback({
+    result[id] = (ctx: DeeGRPC.Context, cb) => {
+      const origin = ctx.metadata.get("origin");
+      if (!havePermision(String(origin[0]), id)) {
+        cb({
           code: grpc.status.PERMISSION_DENIED,
           message: "Permission denied"
         });
         return;
       }
-      tryWrapHandler(fn)(ctx, callback);
+      fn(ctx)
+        .then(res => cb(null, res))
+        .catch(cb);
     };
   });
+  return result;
 }
 
 async function createClients(
-  options: DeeGRPC.ServiceOptions
-): Promise<DeeGRPC.GrpcClientMap> {
-  const clients: DeeGRPC.GrpcClientMap = {};
-  const { clientProtoFile, getClientUri = v => v } = options.args;
-  const { ns, name } = options.srvs.$config;
+  ctx: Dee.ServiceInitializeContext,
+  args: DeeGRPC.Args
+): Promise<DeeGRPC.ClientMap> {
+  const clients: DeeGRPC.ClientMap = {};
+  const {
+    clientProtoFile,
+    getClientUri = () => DEFAULT_HOST + ":" + DEFAULT_PORT
+  } = args;
+  const { ns, name } = ctx.srvs.$config;
   const protoRoot = loadProtoFile(clientProtoFile);
   const proto = protoRoot[ns];
   Object.keys(proto).forEach(serviceName => {
@@ -144,31 +145,19 @@ async function createClients(
       getClientUri(serviceName),
       grpc.credentials.createInsecure()
     );
-    client.call = (
-      funcName: string,
-      args: any,
-      metadata?: DeeGRPC.Metadata,
-      callback?: (data: any) => void
-    ) => {
-      metadata.origin = name;
+    client.call = (funcName: string, args: any, metadata?: grpc.Metadata) => {
+      if (!metadata) metadata = new grpc.Metadata();
+      metadata.add("origin", name);
       const fn = client[funcName];
       const unSupportedRes = {
         message: serviceName + "." + funcName + " is not supported",
         status: grpc.status.UNIMPLEMENTED
       };
-      if (callback) {
-        if (!fn) {
-          callback(unSupportedRes);
-          return;
-        }
-        fn.call(client, args, metadata, callback);
-      }
-      if (!fn) {
-        return Promise.resolve(unSupportedRes);
-      }
-      return new Promise(resolve => {
-        fn.call(client, args, metadata, data => {
-          resolve(data);
+      return new Promise((resolve, reject) => {
+        if (!fn) return reject(unSupportedRes);
+        client[funcName](args, metadata, (err, res) => {
+          if (err) return reject(err);
+          resolve(res);
         });
       });
     };
@@ -179,17 +168,6 @@ async function createClients(
 
 function loadProtoFile(filename: string): grpc.GrpcObject {
   return grpc.loadPackageDefinition(grpcLoader.loadSync(filename));
-}
-
-function tryWrapHandler(fn: DeeGRPC.HandlerFunc): DeeGRPC.HandlerFunc {
-  const type = Object.prototype.toString.call(fn);
-  if (type === "[object AsyncFunction]") {
-    return (ctx: DeeGRPC.Context, callback?: (result: any) => void) => {
-      const fnReturn = fn(ctx, callback);
-      Promise.resolve(fnReturn).catch(callback);
-    };
-  }
-  return fn;
 }
 
 export = DeeGRPC;
