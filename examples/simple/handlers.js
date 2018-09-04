caadd12156f8a1a47f79d3adfaaee48a07dd4e5b
Sigoden Huang <sigoden@gmail.com>
2018-09-04 10:07:03 +0800

    clear for merge refactor repo

--- examples/simple/handlers.js
+++
@@ -1,6 +0,0 @@
-function hello(req, res, next) {
-  var name = req.params.name || 'stranger';
-  res.json(name);
-}
-
-module.exports = { hello };
--- test/fixtures/handlers/hello.js
+++
@@ -1,7 +0,0 @@
-function hello(req, res, next) {
-  var name = req.query.name || 'stranger';
-  res.json(name);
-  next();
-}
-
-module.exports = hello;
