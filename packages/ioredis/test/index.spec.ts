0d17df005fb1e340a26b7809c2c1af84fff52192
Sigoden Huang <sigoden@gmail.com>
2018-09-04 10:02:46 +0800

    (ioredis) update test

--- packages/ioredis/test/index.spec.ts
+++ packages/ioredis/test/index.spec.ts
@@ -1,15 +1,15 @@
-import * as DeeRedis from "../src";
+import * as DeeIORedis from "../src";
 import { initApp, HANDLERS } from "@sigodenjs/dee-test-utils";
 
 test("should create grpc service", async () => {
-  const serviceOptions = <DeeRedis.ServiceOptions>{
-    initialize: DeeRedis,
+  const serviceOptions = <DeeIORedis.ServiceOptions>{
+    initialize: DeeIORedis,
     args: {
       port: 6479
     } 
   };
   const app = await initApp(HANDLERS, { redis: serviceOptions });
-  const srv = <DeeRedis.Service>app.srvs.redis;
+  const srv = <DeeIORedis.Service>app.srvs.redis;
   const result = await srv.ping()
   expect(result).toEqual('PONG');
   await srv.quit()
