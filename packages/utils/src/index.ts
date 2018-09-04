c502e9e0d68ec442b30cb9ee01db9e825cfda419
Sigoden Huang <sigoden@gmail.com>
2018-09-04 14:31:46 +0800

    (utils) update index.ts

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -5,22 +5,26 @@ interface ModuleMap {
   [k: string]: any;
 }
 
-export function mergedRequireDir(dir: string): ModuleMap {
-  const names = fs.readdirSync(dir);
+export function requireDir(moduleDir: string): ModuleMap {
+  const names = fs.readdirSync(moduleDir);
   const result = {};
   for (const name of names) {
-    const fp = path.join(dir, name);
+    const fp = path.join(moduleDir, name);
     const stat = fs.statSync(fp);
     if (stat.isDirectory()) {
       if (result[name]) {
-        throw new Error(`fail to require dir ${fp}, properity conflict`);
+        throw new Error(
+          `fail to require module directory ${fp}, properity conflict`
+        );
       }
-      result[name] = mergedRequireDir(fp);
+      result[name] = requireDir(fp);
     } else if (stat.isFile()) {
       const fm = require(fp);
       Object.keys(fm).forEach(k => {
         if (result[k]) {
-          throw new Error(`fail to merge mdoule ${fp}, properity conflict`);
+          throw new Error(
+            `fail to require mdoule file ${fp}, properity conflict`
+          );
         }
         result[k] = fm[k];
       });
@@ -35,18 +39,4 @@ export function requireSafely(modulePath: string): any {
     result = require(modulePath);
   } catch (err) {}
   return result;
-}
-
-export function camelCaseToSnakeCase(name: string): string {
-  return String(name)
-    .split("")
-    .map(c => {
-      const code = c.charCodeAt(0);
-      if (code >= 65 && code <= 90) {
-        return "_" + c.toLocaleLowerCase();
-      }
-      return c;
-    })
-    .join("")
-    .replace(/^_/, "");
 }
