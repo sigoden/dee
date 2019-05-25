797e7b540ec8683de37b47e432553a600da73cfe
sigoden <sigoden@gmail.com>
2019-05-25 17:15:24 +0800

    feat: simplify redis

--- packages/ioredis/src/index.ts
+++ packages/ioredis/src/index.ts
@@ -1,29 +1,15 @@
 import * as Dee from "@sigodenjs/dee";
 import * as Redis from "ioredis";
 
-const SEP = ":";
 
-interface ServiceExt {
-  deeSep: string;
-  deeKey(...names: string[]): string;
-}
-
-export type Service<T = {}> = Dee.Service & Redis.Redis & ServiceExt & T;
+export type Service<T = {}> = Dee.Service & Redis.Redis & T;
 
 export type ServiceOptions = Dee.ServiceOptionsT<Args>;
 
-export interface Args extends Redis.RedisOptions {
-  sep?: string;
-}
+export interface Args extends Redis.RedisOptions {}
 
 export async function init<T>(ctx: Dee.ServiceInitializeContext, args: Args): Promise<Service<T>> {
-  const sep = args ? (args.sep ? args.sep : SEP) : SEP;
-  const srv = Object.assign(new Redis(args), {
-    deeSep: sep,
-    deeKey: (...names: string[]) => {
-      return [ctx.srvs.$config.name, ...names].join(srv.deeSep);
-    }
-  });
+  const srv = new Redis(args);
   return new Promise<Service<T>>((resolve, reject) => {
     srv.once("connect", () => resolve(srv as Service<T>));
     srv.once("error", err => reject(err));
