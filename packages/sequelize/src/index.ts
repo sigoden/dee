44a4c4e92f7dd915de1d85094383e5194649bc8f
sigoden <sigoden@gmail.com>
2019-10-17 16:52:34 +0800

    feat(sequelize): optmize

--- packages/sequelize/src/index.ts
+++ packages/sequelize/src/index.ts
@@ -1,26 +1,61 @@
-import { Sequelize, Options, Model } from "sequelize";
-import { SrvContext, ServiceBase, Ctor, STOP_KEY } from "@sigodenjs/dee-srv";
+import { Sequelize as Sequelize_, Options, Model, ModelCtor } from "sequelize";
+import { SrvContext, ServiceBase, Ctor, STOP_KEY, INIT_KEY } from "@sigodenjs/dee-srv";
 
 
-export type Service<T extends Sequelize, U> = ServiceBase & T & {
-  model<TKey extends keyof U>(name: TKey): U[TKey];
-  models: U;
-};
+export type Service<T extends Sequelize<U>, U extends ModelMap> =  T;
 
-export interface Args {
+export interface Args<U> {
   database: string;
   username: string;
   password: string;
+  models: U;
   options?: Options;
 }
 
 
-export async function init<T extends Sequelize, U>(ctx: SrvContext, args: Args, ctor?: Ctor<T>): Promise<Service<T, U>> {
+export async function init<T extends Sequelize<U>, U extends ModelMap>(ctx: SrvContext, args: Args<U>, ctor?: Ctor<T>): Promise<Service<T, U>> {
   const { database, username, password, options: connectOptions } = args;
   const srv = new (ctor || Sequelize)(database, username, password, connectOptions);
-  await srv.authenticate();
-  srv[STOP_KEY] = () => srv.close();
+  srv.args = args;
+  srv.ctx = ctx;
   return srv as Service<T, U>;
+}
+
+export interface ModelMap {
+  [k: string]: ModelCtor<Model<any, any>>;
+}
+
+export const LOAD_MODELS = Symbol("loadModel");
+
+export class Sequelize<U extends ModelMap> extends Sequelize_ implements ServiceBase {
+  public ctx: SrvContext;
+  public args: Args<U>;
+  public models: U;
+  public async [INIT_KEY]() {
+    await this.authenticate();
+    await this[LOAD_MODELS]();
+  }
+
+  public model<TKey extends keyof U>(name: TKey): U[TKey] {
+    return super.model(name as any) as any;
+  }
+  public async [LOAD_MODELS]() {
+    for (const key in this.args.models) {
+      const model = this.args.models[key] as any;
+      await model.bootstrap(this);
+      model.srvs = this.ctx.srvs;
+      if (this.models[key]) {
+        (this.models as any)[key] = model;
+      }
+    }
+    for (const key in this.models) {
+      const model = this.args.models[key] as any;
+      if (model["associate"]) model["associate"]();
+    }
+  }
+  public [STOP_KEY]() {
+    return this.close();
+  }
 }
 
 export type ModelT<T> = { new(): T } & typeof Model;
--- packages/sequelize/test/index.spec.ts
+++ packages/sequelize/test/index.spec.ts
@@ -1,22 +1,24 @@
 /* eslint-disable @typescript-eslint/no-namespace, @typescript-eslint/no-empty-interface */
-import { Model, QueryTypes, Sequelize } from "sequelize";
-import { STOP_KEY } from "@sigodenjs/dee-srv";
+import { Model, QueryTypes } from "sequelize";
+import { STOP_KEY, INIT_KEY } from "@sigodenjs/dee-srv";
 import { createSrvLite } from "@sigodenjs/dee-srv-test-utils";
 import * as DeeSequelize from "../src";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-class User extends Model {}
-interface Models {
-  User: User;
-}
+class User extends Model { }
+const ModelMap = {
+  User: User,
+};
+
 
 test("should create sequelize service", async () => {
-  const srv = await createSrvLite<DeeSequelize.Service<Sequelize, Models>, DeeSequelize.Args>("errs", {
+  const srv = await createSrvLite<DeeSequelize.Service<DeeSequelize.Sequelize<typeof ModelMap>, typeof ModelMap>, DeeSequelize.Args<typeof ModelMap>>("errs", {
     initialize: DeeSequelize.init,
     args: {
       database: "mysql",
       username: "root",
       password: "mysql",
+      models: ModelMap,
       options: {
         port: 3306,
         dialect: "mysql",
